41d8ca20c1b0dffffbea364887c5b2d06ccc53c0
Sanilal P Sathyan <sanilal@gmail.com>
2024-03-30 09:04:25 +0400

    home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,10 @@
+import HomeHeader from '@/components/HomeHeader'
 import React from 'react'
 
 export default function Home() {
   return (
-    <h1 className="text-red-600">Home</h1>
+   <>
+   <HomeHeader />
+   </>
   )
 }
