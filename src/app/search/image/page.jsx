049baf28ec004fe0701bc5765463f4279a690eef
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-05 16:52:10 +0400

    Pagination

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,7 +1,8 @@
 import ImageSearchResults from '@/components/ImageSearchResults';
 import Link from 'next/link'
 export default async function ImageSearchPage({searchParams}) {
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}x&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image`);
+  const startIndex = searchParams.start || "1";
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`);
   if(!response.ok) throw new Error("Something went wrong")
   const data = await response.json();
   const results = data.items; 
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,7 +1,8 @@
 import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link'
 export default async function WebSearchPage({searchParams}) {
-  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
+  const startIndex = searchParams.start || "1";
+  const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`);
   if(!response.ok) throw new Error("Something went wrong")
   const data = await response.json();
   const results = data.items; 
