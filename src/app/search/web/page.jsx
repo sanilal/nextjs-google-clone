8c05af82de3586a2ed8d48627f73c26d39cc2156
Sanilal P Sathyan <sanilal@gmail.com>
2024-04-05 09:13:10 +0400

    search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link'
 export default async function WebSearchPage({searchParams}) {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
@@ -20,7 +21,7 @@ export default async function WebSearchPage({searchParams}) {
   }
   return (
     <div>
-      {results && results.map((result)=> <h1>{result.title}</h1>)}
+      {results && <WebSearchResults results={data} />}
     </div>
   )
 }
--- src/components/SearchHeaderOptions.jsx
+++ src/components/SearchHeaderOptions.jsx
@@ -11,7 +11,7 @@ export default function SearchHeaderOptions() {
         router.push(`/search/${tab=== "Images" ? "image" : "web"}?searchTerm=${searchTerm}`)
     }
   return (
-    <div className="flex space-x-2 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
+    <div className="flex space-x-2 mt-10 select-none border-b w-full justify-center lg:justify-start lg:pl-52 text-gray-700 text-sm">
         <div onClick={()=>selectTab('All')} className={`flex items-center space-x-1 border-b-4 border-transparent active:text-blue-500 cursor-pointer pb-3 px-2 ${pathname === '/search/web' && '!text-blue-600 !border-blue-600'}`}>
         <AiOutlineSearch className="text-md"/>
         <p>All</p>
